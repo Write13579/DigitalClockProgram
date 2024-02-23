9ac96fd84ce37f0d222d61203744ba77941d6d9a
Patryk Baraniak <pb311239@student.polsl.pl>
2024-02-23 03:21:31 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+function updateClock() {
+  let clock = document.getElementById(`clock`) as HTMLDivElement;
+
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  clock.textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1);
